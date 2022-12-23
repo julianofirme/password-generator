d44e99958a376844a2ea4eb78605c74d61483107
Juliano <julianofirme23@gmail.com>
2022-12-22 21:32:08 -0300

    chore: project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import GlobalStyles from "./styles/GlobalStyles"
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyles />
+    </>
+  )
+}
+
+export default App
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html {
+    min-height: 100%;
+    background: var(--color-primary);
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 600;
+  }
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: 'Montserrat', -apple-system,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif;
+  }
+  :root {
+    --color-primary: #A4FFAE;
+    --color-black: #333;
+    --color-white: #FFF;
+    --color-gray: #E2E6E9;
+
+    --color-bg: #101014;
+    --color-bg-container: #24232A;
+    --color-border: #ddd;
+
+    --strength-weak: #FF6961;
+    --strength-medium: #F5CA6E;
+    --strength-strong: #A4FFAE;
+
+    --color-text: #9A99A0;
+  }
+`;
