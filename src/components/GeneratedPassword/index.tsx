db0d2e2bfa4171d6a1b964ebc738fff87409d91b
Juliano <julianofirme23@gmail.com>
2023-01-05 19:32:35 -0300

    feat: implement the copy to clipboard button

--- src/components/GeneratedPassword/index.tsx
+++ src/components/GeneratedPassword/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { MdOutlineFileCopy } from 'react-icons/md';
-import { useRecoilValue } from 'recoil';
-import { passwordState } from '../../atoms/password';
+import React from "react";
+import { MdOutlineFileCopy } from "react-icons/md";
+import { useRecoilValue } from "recoil";
+import { passwordState } from "../../atoms/password";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 export function GeneratedPassword() {
-  const password = useRecoilValue(passwordState)
+  const password = useRecoilValue(passwordState);
 
   return (
     <Container>
       <h3>{password}</h3>
-      <button><MdOutlineFileCopy /></button>
+      <button
+        onClick={() => {
+          navigator.clipboard.writeText(password);
+        }}
+      >
+        <MdOutlineFileCopy />
+      </button>
     </Container>
   );
 }
